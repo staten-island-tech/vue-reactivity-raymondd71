7def90dfc719e17d4d3afd82787e28c7affb9456
raymondd71 <raymondd71@nycstudents.net>
2025-02-10 12:31:36 -0500

    adsasd

--- app/src/components/SpikeCard.vue
+++ app/src/components/SpikeCard.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="card border-gray-300 rounded-lg p-4 text-center w-90">
     <img class="w-full h-auto rounded-md" :src="spike.picture" :alt="spike.name" />
+    <!-- change size of
+    picture -->
     <h2 class="text-lg font-semibold mt-2">{{ spike.name }}</h2>
     <p>Type: {{ props.spike.type }}</p>
     <p>Price: ${{ spike.price }}</p>
