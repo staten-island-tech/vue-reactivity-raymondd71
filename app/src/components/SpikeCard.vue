fb70b609cf0b63d423ccdf1d84abfbf585206691
raymondd71 <raymondd71@nycstudents.net>
2025-02-10 12:30:02 -0500

    SAdjasidjas
    Change size of image so it is the same
    Add images to each spike

--- app/src/components/SpikeCard.vue
+++ app/src/components/SpikeCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="card border-gray-300 rounded-lg p-4 text-center w-36">
-    <img class="w-full h-auto rounded-md" :src="spikes.picture" :alt="spike.name" />
+  <div class="card border-gray-300 rounded-lg p-4 text-center w-90">
+    <img class="w-full h-auto rounded-md" :src="spike.picture" :alt="spike.name" />
     <h2 class="text-lg font-semibold mt-2">{{ spike.name }}</h2>
-    <p>{{ spike.type.join(',') }}</p>
+    <p>Type: {{ props.spike.type }}</p>
     <p>Price: ${{ spike.price }}</p>
   </div>
 </template>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,16 +2,16 @@
 import LogIn from '../components/LogIn.vue'
 import AnimalCards from '../components/AnimalCards.vue'
 import SpikeCard from '../components/SpikeCard.vue'
-import trackSpikes from '@/components/products' // remove this to fix
+import { trackSpikes } from '@/components/products'
 </script>
 
 <template>
   <main>
-    <div class="container">
+    <AnimalCards class="text-[50px]" />
+    <div class="container flex flex-wrap gap-4 justify-center">
       <SpikeCard v-for="spike in trackSpikes" :spike="spike" :key="spike.name" />
     </div>
-    <TheWelcome />
-    <AnimalCards class="text-[50px]" />
-    <LogIn class="text-red-500" title="Spikes" />
+
+    <LogIn class="text-red-500" title="Help me" />
   </main>
 </template>
