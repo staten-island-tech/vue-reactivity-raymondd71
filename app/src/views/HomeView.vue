5cd98af3a6a0e292d2cbd52a089a4b6c088784cd
raymondd71 <raymondd71@nycstudents.net>
2025-02-11 12:32:40 -0500

    added some spikes

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup>
 import LogIn from '../components/LogIn.vue'
-import AnimalCards from '../components/AnimalCards.vue'
+import TitleHead from '../components/TitleHead.vue'
 import SpikeCard from '../components/SpikeCard.vue'
 import { trackSpikes } from '@/components/products'
 </script>
 
 <template>
   <main>
-    <AnimalCards class="text-[50px]" />
+    <TitleHead class="text-[50px]" />
     <div class="container flex flex-wrap gap-4 justify-center">
       <SpikeCard v-for="spike in trackSpikes" :spike="spike" :key="spike.name" />
     </div>
