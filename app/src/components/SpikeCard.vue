69673343cb9f2e76d888e63a8c36ac6e68c97699
raymondd71 <raymondd71@nycstudents.net>
2025-02-12 12:32:37 -0500

    asdsa

--- app/src/components/SpikeCard.vue
+++ app/src/components/SpikeCard.vue
@@ -5,18 +5,25 @@
     picture -->
     <h2 class="text-lg font-semibold mt-2">{{ spike.name }}</h2>
     <p>Type: {{ props.spike.type }}</p>
-    <p>Price: ${{ spike.price }}</p>
+    <p class="price">Price: ${{ spike.price }}</p>
+    <button @click="" class="border-2">Add to Cart</button>
+    <!-- remove click for it to work -->
   </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
 const props = defineProps({
   spike: {
     type: Object,
     required: true,
   },
 })
+
+function addCart{
+let cart = []
+let totalPrice = 0
+
+};
 </script>
 
 <style></style>
