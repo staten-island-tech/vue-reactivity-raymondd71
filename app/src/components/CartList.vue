591f2505c2a948ac8d0216fb7ec51aeafce2b721
raymonddong71 <raymonddong71@gmail.com>
2025-02-21 17:39:21 -0500

    sda

--- app/src/components/CartList.vue
+++ app/src/components/CartList.vue
@@ -3,7 +3,7 @@
     <h2 class="text-lg font-semibold mt-2">{{ spike.name }}</h2>
     <p class="text-[18px] mt-2">Type: {{ props.spike.type }}</p>
     <p class="text-[18px] mt-2">Price: ${{ spike.price }}</p>
-    <button @click="remove($event)" :data-id="spike.id" class="text-[15px] mt-2">Remove</button>
+    <button @click="remove()" class="text-[15px] mt-2">Remove</button>
     <!-- Add a button to remove the item -->
   </div>
 </template>
@@ -13,13 +13,8 @@ const props = defineProps({
   spike: Object,
 })
 import { cart } from '@/components/cart'
-function remove(event) {
-  const spikeId = Number(event.target.getAttribute('data-id')) // Get ID from button
-  const index = cart.findIndex((item) => item.id === spikeId)
-
-  if (index !== -1) {
-    cart.splice(index, 1)
-  }
+function remove() {
+  cart.splice(props.id, 1)
 }
 </script>
 
