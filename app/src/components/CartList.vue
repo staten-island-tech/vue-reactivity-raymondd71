d6c49c5f5145778b336897bc2d376bd004c683cd
raymonddong71 <raymonddong71@gmail.com>
2025-02-21 16:05:54 -0500

    sas

--- app/src/components/CartList.vue
+++ app/src/components/CartList.vue
@@ -3,7 +3,7 @@
     <h2 class="text-lg font-semibold mt-2">{{ spike.name }}</h2>
     <p class="text-[18px] mt-2">Type: {{ props.spike.type }}</p>
     <p class="text-[18px] mt-2">Price: ${{ spike.price }}</p>
-    <button class="text-[15px] mt-2">Remove</button>
+    <button @click="remove()" class="text-[15px] mt-2">Remove</button>
     <!-- Add a button to remove the item -->
   </div>
 </template>
@@ -12,6 +12,10 @@
 const props = defineProps({
   spike: Object,
 })
+import { cart } from '@/components/cart'
+function remove() {
+  cart.splice(props.index, 1) // change this so that it only removes the card that the button is pressed from.
+}
 </script>
 
 <style scoped></style>
