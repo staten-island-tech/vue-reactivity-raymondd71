ee997688914a861c6e8e02409fbfdee35bf1395b
raymonddong71 <raymonddong71@gmail.com>
2025-02-21 17:40:10 -0500

    kkmij
    sasa

--- app/src/components/CartList.vue
+++ app/src/components/CartList.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 import { cart } from '@/components/cart'
 function remove() {
   const index = cart.findIndex((key) => key.name === this.props.spike.id)
-  cart.splice(props.spike.index, 1) // change this so that it only removes the card that the button is pressed from.-
+  cart.splice(index, 1) // change this so that it only removes the card that the button is pressed from.-
 
   /*const target = update.cart.findIndex((key) => key.name === this.product.name);
 
